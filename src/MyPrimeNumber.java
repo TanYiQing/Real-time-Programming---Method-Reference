dd5513bd2dcda53401a2f89583a4b58a8848fd74
TanYiQing <yiqingtan99@gmail.com>
2021-11-05 20:58:22 +0800

    tutorial3

--- src/MyPrimeNumber.java
+++ src/MyPrimeNumber.java
@@ -1,6 +1,53 @@
-public class MyPrimeNumber {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class MyPrimeNumber {
+    static int x,y,z,total;
+
+    public static void main(String[] args){
+	Scanner scan=new Scanner(System.in);
+    System.out.print("Please input x:");
+    x=scan.nextInt();
+    Thread t1=new Thread(MyPrimeNumber::thread1);
+    Thread t2=new Thread(MyPrimeNumber::thread2);
+    t1.start();
+    t2.start();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.print("Total: "+total);
+    }
+
+    public static void thread1(){
+        y=x+5;
+        for(int i=x;i<=y;i++){
+            if(isPrime(i)){
+                System.out.println(Thread.currentThread().getName()+ ": "+i);
+                total+=i;
+            }
+        }
+    }
+
+    public static void thread2(){
+        z=x+10;
+        for(int i=y;i<=z;i++){
+            if(isPrime(i)){
+                System.out.println(Thread.currentThread().getName()+ ": "+i);
+                total+=i;
+            }
+        }
+    }
+
+    public static boolean isPrime(int checknum){
+        if (checknum <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(checknum); i++) {
+            if (checknum % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
